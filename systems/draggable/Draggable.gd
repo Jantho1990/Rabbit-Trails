a71ab38b448994a069ed1fbca467e4b8b4757123
Joshua Anthony <jantho1990@gmail.com>
2019-11-05 21:25:50 -0600

    added Draggable system; modified selection demo to allow units to be dragged when selected

---
+++ systems/draggable/Draggable.gd
@@ -0,0 +1,21 @@
+extends Node
+
+# Can the entity be dragged right now.
+var allowed_to_drag = false
+
+onready var parent = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if allowed_to_drag:
+		if not Selection.is_entity_selected(parent):
+			allowed_to_drag = false
+		else:
+			parent.position = get_viewport().get_mouse_position()
+	else:
+		if Selection.is_entity_selected(parent):
+			allowed_to_drag = true
