811ab095040f2b268f40e1b3efe00f3f8d729225
Joshua Anthony <jantho1990@gmail.com>
2019-11-07 22:54:37 -0600

    fixed bug where draggable did not respect units without placeable

--- systems/draggable/Draggable.gd
+++ systems/draggable/Draggable.gd
@@ -35,7 +35,7 @@ func _physics_process(delta):
 	if placeable:
 		allowed_to_move = placeable.allowed_to_move
 	else:
-		allowed_to_move == true
+		allowed_to_move = true
 	
 	if parent_allowed_to_drag and allowed_to_move and allowed_to_drag:
 		if not Selection.is_entity_selected(parent):
