144dafb1a6d3b2068254dede7487db3ce3f085bb
Joshua Anthony <jantho1990@gmail.com>
2019-11-09 14:45:14 -0600

    testing placement area collision using kinematicbody2d

--- systems/placeable/PlacementArea.gd
+++ systems/placeable/PlacementArea.gd
@@ -4,6 +4,8 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+const UP = Vector2(0, -1)
+
 onready var root = owner
 
 # Called when the node enters the scene tree for the first time.
@@ -17,8 +19,24 @@ func _ready():
 func _physics_process(delta):
 	position = owner.position
 #	print("placement area position: ", position)
-	var collision = move_and_collide(Input.get_last_mouse_speed() * delta)
+	var motion = move_and_slide(Input.get_last_mouse_speed(), UP)
+	var slide_count = get_slide_count()
+	var collision = false
+	for i in get_slide_count():
+		var collider = get_slide_collision(i)
+		if collider:
+			collision = true
+			print("Collided with ", collider.collider.name)
+	
 	if collision:
-		print("collided!")
+#		print("collided!")
+		if is_on_floor():
+			print("is on floor")
+		elif is_on_wall():
+			print("is on wall")
+		elif is_on_ceiling():
+			print("is on ceiling")
+		else:
+			print("collision")
 	else:
 		print("nope")
