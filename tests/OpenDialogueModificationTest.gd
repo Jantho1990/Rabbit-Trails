70c4d836bf57d5e8417401b2ca438a2945ae1a72
Joshua Anthony <jantho1990@gmail.com>
2019-11-21 19:33:49 -0600

    added open_dialogue to the game; created OpenDialogueModificationTest to contain test changes to see if open_dialogue can work with this game

---
+++ tests/OpenDialogueModificationTest.gd
@@ -0,0 +1,13 @@
+extends Node
+
+onready var dialogue = $InterfaceLayer/UI/Dialogue
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print('ready')
+	dialogue.initiate('test_dialogue_1')
+	print('boom headshot')
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		dialogue.next()
