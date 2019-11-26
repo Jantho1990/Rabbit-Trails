3b134a1ac86d6d751aaaa8a0531adb011f23c1a7
Joshua Anthony <jantho1990@gmail.com>
2019-11-25 22:19:42 -0600

    wired up TimeBar; added stage time tracking to StageManager

--- managers/StageManager.gd
+++ managers/StageManager.gd
@@ -12,6 +12,10 @@ export(Array, String, FILE, '*Stage.tscn') var stages = []
 
 var loaded_stages = []
 
+var track_stage_time = false
+var stage_time = 0.00
+var stage_time_step = 0.0
+
 func _init():
 #	current_stage_index = UserData.get('current_stage_index')
 	current_stage_name = UserData.get('current_stage_name')
@@ -26,7 +30,12 @@ func _ready():
 			loaded_stages.push_back(loaded_stage)
 	load_stage(get_stage_with_name(current_stage_name))
 
+func _process(delta):
+	if track_stage_time:
+		update_stage_time(delta)
+
 func _on_Victory(data):
+	stop_stage_time()
 	if data.has('next_stage'):
 		next_stage_name = data.next_stage
 
@@ -48,8 +57,26 @@ func load_stage(stage):
 	current_stage = stage
 	current_stage_name = stage.name
 	add_child(current_stage)
+	start_stage_time()
 
 func unload_current_stage():
 	remove_child(current_stage)
 	current_stage = null
 	current_stage_name = ''
+
+func start_stage_time():
+	track_stage_time = true
+	stage_time = 0.0
+	stage_time_step = 0.0
+	GlobalSignal.dispatch('stage_time_updated', { 'time': int(stage_time) })
+
+func stop_stage_time():
+	track_stage_time = false
+	GlobalSignal.dispatch('stage_time_updated', { 'time': int(stage_time)  })
+
+func update_stage_time(time):
+	stage_time += time
+	stage_time_step += time
+	if stage_time_step >= 1:
+		GlobalSignal.dispatch('stage_time_updated', { 'time': int(stage_time)  })
+		stage_time_step = stage_time_step - 1
---
+++ ui/stat_displays/time_bar/TimeBar.gd
@@ -0,0 +1,19 @@
+extends HBoxContainer
+
+onready var display = $StatBarDisplay
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalSignal.listen('stage_time_updated', self, '_on_Stage_time_updated')
+
+func _on_Stage_time_updated(data):
+	var time = data.time
+	var display_time = ''
+#	if time % 60 >= 1:
+#		display_time = '00:' + String(round(time)).pad_zeros(2)
+##	elif time % 600 >= 1:
+##		display_time = '0' + String(time % 600) + ':' + String(round(time)).pad_zeros(2)
+#	else:
+#		display_time = String(time / 60).pad_zeros(2) + ':' + String(round(time)).pad_zeros(2)
+	display_time = String(round(time / 60)).pad_zeros(2) + ':' + String(time % 60).pad_zeros(2)
+	display.set_value(display_time)
