ba075bb1586d7b646ff9f6a35d33e928fc5c6d6f
Joshua Anthony <jantho1990@gmail.com>
2019-11-29 21:22:54 -0600

    added hover background for build unit buttons

--- ui/buttons/build_unit/BuildUnitButton.gd
+++ ui/buttons/build_unit/BuildUnitButton.gd
@@ -4,10 +4,14 @@ export(String) var unit_name
 export(String, 'Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C') var command_card_key
 
 onready var command_card = owner
-
+onready var hover_color = $HoverColor
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	hover_color.modulate = Color(1, 1, 1, 0)
+	connect('mouse_entered', self, '_on_Mouse_entered')
+	connect('mouse_exited', self, '_on_Mouse_exited')
+	hover_color.connect('mouse_entered', self, '_on_Mouse_entered')
+	hover_color.connect('mouse_exited', self, '_on_Mouse_exited')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -18,6 +22,15 @@ func _input(event):
 		event.is_action('command_card_key') and \
 			char(event.scancode) == command_card_key:
 				GlobalSignal.dispatch('build_unit', { 'unit_name': unit_name })
+
+func _on_Mouse_entered():
+	hover_color.modulate = Color(1, 1, 1, 1)
+
+func _on_Mouse_exited():
+	hover_color.modulate = Color(1, 1, 1, 0)
+
+func flash():
+	pass
 
 func _on_BuildUnitButton_pressed():
 	GlobalSignal.dispatch('build_unit', { 'unit_name': unit_name })
