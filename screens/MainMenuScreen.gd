b60ea00bc260cc2159dbf3f3ea5bf1eea38994ff
Joshua Anthony <jantho1990@gmail.com>
2019-11-30 02:31:54 -0600

    added a main menu screen background

--- screens/MainMenuScreen.gd
+++ screens/MainMenuScreen.gd
@@ -1,12 +1,10 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var background = $MainMenuBackgroundCave
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	background.position = Vector2(rect_size.x / 2, rect_size.y / 2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
