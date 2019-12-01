8cc04c92f46dbd5698978e7c8ef07ac416560d86
Joshua Anthony <jantho1990@gmail.com>
2019-12-01 12:20:30 -0600

    adjusted score calculations

--- scripts/Score.gd
+++ scripts/Score.gd
@@ -11,8 +11,10 @@ const TOTAL_SCORE_KEY = 'total_score'
 
 const RABBIT_ALIVE_SCORE = 10000
 const RABBIT_DEAD_SCORE = -6000
-const BUDGET_SCORE = 10
+const BUDGET_SCORE = 5
 const TIME_SCORE = -(100/60)
+
+const TIME_SCORE_START = 600
 
 func _ready():
 	if UserData.has(TOTAL_SCORE_KEY):
@@ -26,7 +28,7 @@ func calculate_score(rabbits_alive, rabbits_dead, budget_remaining, time_elapsed
 	var raw_score = (rabbits_alive * RABBIT_ALIVE_SCORE) + \
 		(rabbits_dead * RABBIT_DEAD_SCORE) + \
 		(budget_remaining * BUDGET_SCORE) + \
-		(time_elapsed * TIME_SCORE)
+		((time_elapsed * TIME_SCORE) + TIME_SCORE_START)
 	return max(raw_score, 0)
 
 func calculate_rabbits_captured_score(rabbits_captured):
@@ -39,7 +41,7 @@ func calculate_budget_remaining_score(budget_remaining):
 	return budget_remaining * BUDGET_SCORE
 
 func calculate_time_elapsed_score(time_elapsed):
-	return time_elapsed * TIME_SCORE
+	return (time_elapsed * TIME_SCORE) + TIME_SCORE_START
 
 func add_current_score(value):
 	current_score += value
