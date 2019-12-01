7bcce493454139866621f8aa754e66fbdc24fa21
Joshua Anthony <jantho1990@gmail.com>
2019-11-30 19:48:20 -0600

    OptionsModal can now be accessed from the main menu

--- ui/modals/MainMenuModal.gd
+++ ui/modals/MainMenuModal.gd
@@ -1,12 +1,11 @@
 extends VBoxContainer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var change_menu_func # A funcref explicitly passed in from an external source that can change menus.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$NewGameButton.connect('pressed', self, '_on_NewGameButton_pressed')
+	$OptionsButton.connect('pressed', self, '_on_OptionsButton_pressed')
 	$ExitButton.connect('pressed', self, '_on_ExitButton_pressed')
 	
 	if not UserData.has('has_current_game') or UserData.get('has_current_game') == false:
@@ -22,5 +21,13 @@ func _on_NewGameButton_pressed():
 	UserData.set('current_stage_name', 'TutorialStage')
 	get_tree().change_scene('res://screens/GameScreen.tscn')
 
+func _on_OptionsButton_pressed():
+	if change_menu_func:
+		change_menu_func.call_func('OptionsModal')
+
 func _on_ExitButton_pressed():
 	get_tree().quit()
+
+func set_change_menu_func(change_func : Object):
+	if change_func is FuncRef:
+		change_menu_func = change_func
--- ui/modals/MainMenuPopup.gd
+++ ui/modals/MainMenuPopup.gd
@@ -1,13 +1,24 @@
 extends CenterContainer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var main_menu_modal = $MainMenuModal
+onready var options_modal = $OptionsModal
+onready var modals = get_children()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	main_menu_modal.set_change_menu_func(funcref(self, 'change_menu'))
+	options_modal.set_back_func(funcref(self, 'options_back'))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func change_menu(menu_name):
+	for modal in modals:
+		if modal.name == menu_name:
+			modal.show()
+		else:
+			modal.hide()
+
+func options_back():
+	change_menu('MainMenuModal')
