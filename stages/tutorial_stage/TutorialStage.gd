bf0b53188b483ecfb4733ddb515bb993a9730b86
Joshua Anthony <jantho1990@gmail.com>
2019-12-01 10:45:13 -0600

    expanding tutorial script and stage

--- stages/tutorial_stage/TutorialStage.gd
+++ stages/tutorial_stage/TutorialStage.gd
@@ -10,22 +10,25 @@ onready var cinematics = $Cinematics
 onready var triggers = $Triggers
 
 func _ready():
-	GlobalSignal.listen('OpeningDialogue_stopped', self, '_on_OpeningDialogue_stopped')
-#	GlobalSignal.listen('dialogue_finished', self, '_on_Dialogue_finished')
-	triggers.get_trigger('OpeningDialogue').start(1.5)
-#	cinematics.get_mark('LevelStart')
-	cinematics.focus_on_mark('LevelStart')
-#	GlobalSignal.dispatch('victory', { 'next_stage': 'SwitchTestStage' })
-#	GlobalSignal.dispatch('defeat', {})
-	GlobalSignal.listen('RabbitAdvanced_entered', self, '_on_RabbitAdvanced_entered')
 	Budget.set_money(5000)
 	Rabbits.reset()
-	GlobalSignal.dispatch('rabbit_hole_activate')
 	
+	# Dialogue variable resets
 	if PROGRESS.variables.has('skip_tutorial'):
 		PROGRESS.variables.skip_tutorial = false
 	if PROGRESS.variables.has('tutorial_intro_complete'):
 		PROGRESS.variables.tutorial_intro_complete = false
+	
+	# Triggers
+	GlobalSignal.listen('dialogue_finished', self, '_on_Dialogue_finished')
+	GlobalSignal.listen('OpeningDialogue_stopped', self, '_on_OpeningDialogue_stopped')
+	GlobalSignal.listen('RabbitAdvanced_entered', self, '_on_RabbitAdvanced_entered')
+	GlobalSignal.listen('tutorial_cinematic_mark', self, '_on_Cinematic_mark')
+	GlobalSignal.dispatch('rabbit_hole_activate')
+	triggers.get_trigger('OpeningDialogue').start(1.5)
+	
+	# Cinematics
+	cinematics.focus_on_mark('LevelStart')
 
 func _physics_process(delta):
 	if Rabbits.all_rabbits_added and Rabbits.rabbits_alive == 0:
@@ -35,15 +38,33 @@ func _physics_process(delta):
 			'time_elapsed': StageManager.stage_time
 		})
 
+func _on_Dialogue_finished(data):
+	var dialogue = data.dialogue
+	if dialogue.has('file_id'):
+		match dialogue.file_id:
+			'tutorial_0':
+				if not PROGRESS.variables.has('skip_tutorial') or \
+					PROGRESS.variables.skip_tutorial:
+						GlobalSignal.dispatch('dialogue', {
+							'func_name': 'initiate',
+							'file_id': 'tutorial_explanation',
+						})
+
+###
+# Responses to triggers
+###
+
+func _on_Cinematic_mark(data):
+	cinematics.move_to_mark(data.mark_name)
+
 func _on_OpeningDialogue_stopped():
 	GlobalSignal.dispatch('dialogue', {
 		'func_name': 'initiate',
 		'file_id': 'tutorial_0',
 	})
-	cinematics.move_to_mark('OpeningDialogue')
+#	cinematics.move_to_mark('OpeningDialogue')
 
 func _on_RabbitAdvanced_entered(data):
-#	breakpoint
 	if data.collider is Rabbit:
 		if PROGRESS.variables.has('skip_tutorial') and PROGRESS.variables.skip_tutorial:
 			return # We already triggered this.
@@ -54,8 +75,3 @@ func _on_RabbitAdvanced_entered(data):
 					'func_name': 'initiate',
 					'file_id': 'skip_tutorial'
 				})
-
-func _on_Dialogue_finished(data):
-	var dialogue = data.dialogue
-	if dialogue.has('file_id') and dialogue.file_id == 'test_dialogue_1':
-		GlobalSignal.dispatch('victory', { 'next_stage': 'SwitchTestStage' })
