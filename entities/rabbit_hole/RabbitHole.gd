98583cdeeb884cee93468acb791d4ad5fd0d0328
Joshua Anthony <jantho1990@gmail.com>
2019-12-01 03:27:52 -0600

    added new art for rabbit hole; rabbile hole now accepts a hole type to choose which sprite frame to display

--- entities/rabbit_hole/RabbitHole.gd
+++ entities/rabbit_hole/RabbitHole.gd
@@ -1,9 +1,19 @@
 extends EntityGenerator
 
+enum HOLE_TYPES {
+	GRASS,
+	CAVE,
+	MOUNTAIN
+}
+export(HOLE_TYPES) var hole_type = HOLE_TYPES.GRASS
+
+onready var sprite = $Art/Sprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GlobalSignal.listen('rabbit_hole_activate', self, '_on_Rabbit_hole_activate')
 	GlobalSignal.listen('add_entity', self, '_on_Add_entity')
+	sprite.frame = hole_type # The enum values line up with the sprite frame.
 	print('rabbit hole ready')
 
 func _on_Rabbit_hole_activate():
