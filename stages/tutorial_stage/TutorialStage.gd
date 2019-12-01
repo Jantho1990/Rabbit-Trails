12ed06ff074d45b9f0a2557147d829ffea0acc8d
Joshua Anthony <jantho1990@gmail.com>
2019-12-01 14:40:00 -0600

    fixed bugs with loading next stage

--- stages/tutorial_stage/TutorialStage.gd
+++ stages/tutorial_stage/TutorialStage.gd
@@ -30,7 +30,7 @@ func _ready():
 func _physics_process(delta):
 	if Rabbits.all_rabbits_added and Rabbits.rabbits_alive == 0:
 		GlobalSignal.dispatch('end_stage', {
-			'next_stage': 'SwitchTestStage',
+			'next_stage': 'Grass1Stage',
 			'passing_score': passing_score,
 			'time_elapsed': StageManager.stage_time
 		})
