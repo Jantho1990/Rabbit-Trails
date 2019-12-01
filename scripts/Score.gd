89e196581c0cc10d2d235e53caac45b9830bbb82
Joshua Anthony <jantho1990@gmail.com>
2019-11-30 21:07:51 -0600

    scoring logic

--- scripts/Score.gd
+++ scripts/Score.gd
@@ -1,27 +1,42 @@
 extends Node
 
-var points = 0
+###
+# Keeps track of overall game score in a session.
+###
 
-func add(value):
-	points += value
-	EventBus.dispatch("score_updated", { "value": value, "points": points })
+var total_score = 0
+var current_score = 0
 
-func subtract(value):
-	points -= value
-	EventBus.dispatch("score_updated", { "value": value, "points": points })
+const TOTAL_SCORE_KEY = 'total_score'
 
-func multiply(value):
-	points *= value
-	EventBus.dispatch("score_updated", { "value": value, "points": points })
+const RABBIT_ALIVE_SCORE = 10000
+const RABBIT_DEAD_SCORE = -6000
+const BUDGET_SCORE = 10
+const TIME_SCORE = -(100/60)
 
-func divide(value):
-	points /= value
-	EventBus.dispatch("score_updated", { "value": value, "points": points })
+func _ready():
+	if UserData.has(TOTAL_SCORE_KEY):
+		total_score = UserData.get(TOTAL_SCORE_KEY)
+	else:
+		UserData.set(TOTAL_SCORE_KEY, 0)
 
-func set_points(value):
-	points = value
-	EventBus.dispatch("score_set", { "value": value })
+func calculate_score(rabbits_alive, rabbits_dead, budget_remaining, time_elapsed):
+	var raw_score = (rabbits_alive * RABBIT_ALIVE_SCORE) + \
+		(rabbits_dead * RABBIT_DEAD_SCORE) + \
+		(budget_remaining * BUDGET_SCORE) + \
+		(time_elapsed * TIME_SCORE)
+	return min(raw_score, 0)
 
-func reset():
-	points = 0
-	EventBus.dispatch("score_reset")
+func add_current_score(value):
+	current_score += value
+
+func reset_current_score():
+	current_score = 0
+
+func add_total_score(value):
+	total_score += value
+	UserData.set(TOTAL_SCORE_KEY, total_score)
+
+func reset_total_score():
+	total_score = 0
+	UserData.set(TOTAL_SCORE_KEY, 0)
